2d64b508868fb675852a3b8a9f83431ca76184d5
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-08 20:02:58 +0300

    Add nav to button element

--- element/button/button.view.ts
+++ element/button/button.view.ts
@@ -16,6 +16,30 @@ namespace $.$$ {
 			return String( this.state().sub('button_type').value(next) ?? super.button_type() )
 		}
 
+		click() {
+			const action = this.Nav().nav_action()
+			const target_page = this.Nav().nav_target_page()
+			const target_link = this.Nav().nav_target_link()
+
+			if (action === 'none') return
+
+			if (action === 'open') {
+				this.Nav().nav_page_open( target_page )
+			}
+
+			if (action === 'close') {
+				this.Nav().nav_page_close( target_page || this.page().id() )
+			}
+
+			if (action === 'replace') {
+				this.Nav().nav_page_replace( target_page )
+			}
+
+			if (action === 'external') {
+				$mol_dom_context.open(target_link, '_blank')
+			}
+		}
+
 	}
 
 }
