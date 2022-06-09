bcf1b72e472e5a132d6675b6b7b4d2acb2a19395
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 21:15:25 +0300

    _element_speck: Added

---
+++ element/speck/speck.view.ts
@@ -0,0 +1,21 @@
+namespace $.$$ {
+
+	export class $hyoo_sketch_element_speck extends $.$hyoo_sketch_element_speck {
+
+		speck_value(next?: string) {
+			return String(this.state().sub('speck_value').value(next) ?? this.speck_value_default())
+		}
+
+		duplicate(elem?: $hyoo_sketch_element) {
+			const element = elem ?? super.duplicate()
+			const obj = new $hyoo_sketch_element_speck
+			obj.element = $mol_const(element)
+
+			obj.speck_value(this.speck_value())
+
+			return element
+		}
+
+	}
+
+}
