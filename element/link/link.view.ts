89f0052f4db1124a960d2aea1e655522b421f8ff
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 10:46:34 +0300

    Fixes

--- element/link/link.view.ts
+++ element/link/link.view.ts
@@ -8,19 +8,8 @@ namespace $.$$ {
 			return action === 'external' || action === 'none' ? this.Link_external() : this.Link_internal()
 		}
 
-		link_sub() {
-			return [
-				... this.Icon().icon_name() ? [ this.Icon().Icon() ] : [],
-				this.link_title(),
-			]
-		}
-
 		link_hint(next?: string) {
 			return String( this.state().sub('link_hint').value(next) ?? super.link_hint() )
-		}
-
-		link_title(next?: string) {
-			return String( this.state().sub('link_title').value(next) ?? super.link_title() )
 		}
 
 		@ $mol_mem
