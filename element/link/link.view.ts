e2bb23d6eb63859b04e17d495b56b9a018bd0426
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 10:00:04 +0300

    _element_link: Nav support

--- element/link/link.view.ts
+++ element/link/link.view.ts
@@ -2,6 +2,12 @@ namespace $.$$ {
 
 	export class $hyoo_sketch_element_link extends $.$hyoo_sketch_element_link {
 
+		Element() {
+			const action = this.Nav().nav_action()
+
+			return action === 'external' || action === 'none' ? this.Link_external() : this.Link_internal()
+		}
+
 		link_sub() {
 			return [
 				... this.Icon().icon_name() ? [ this.Icon().Icon() ] : [],
@@ -17,6 +23,39 @@ namespace $.$$ {
 			return String( this.state().sub('link_title').value(next) ?? super.link_title() )
 		}
 
+		@ $mol_mem
+		link_arg() {
+			const action = this.Nav().nav_action()
+			const target_page = this.Nav().nav_target_page()
+			const target_link = this.Nav().nav_target_link()
+
+			let link = [] as string[]
+
+			const pages = this.Nav().nav_pages()
+
+			if (action === 'open') {
+				link = [...pages.filter(id => id !== target_page), target_page]
+			}
+
+			if (action === 'close') {
+				link = pages.filter( id => id !== target_page )
+			}
+
+			if (action === 'replace') {
+				link = pages.map( id => id === this.page().id() ? target_page : id )
+			}
+
+			console.log({ link })
+
+			return {
+				[this.Nav().nav_pages_param()]: link.join(','),
+			}
+		}
+
+		@ $mol_mem
+		link_uri() {
+			return this.Nav().nav_action() === 'external' ? this.Nav().nav_target_link() : '#'
+		}
 
 	}
 
