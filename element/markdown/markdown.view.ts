f0312c262b2741bd2599d8ec1ad77a347449228a
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 20:52:37 +0300

    _element_markdown: Added

---
+++ element/markdown/markdown.view.ts
@@ -0,0 +1,21 @@
+namespace $.$$ {
+
+	export class $hyoo_sketch_element_markdown extends $.$hyoo_sketch_element_markdown {
+
+		text(next?: string) {
+			return this.state().sub('markdown_text').text(next) || this.text_default()
+		}
+
+		duplicate(elem?: $hyoo_sketch_element) {
+			const element = elem ?? super.duplicate()
+			const obj = new $hyoo_sketch_element_markdown
+			obj.element = $mol_const(element)
+
+			obj.text(this.text())
+
+			return element
+		}
+
+	}
+
+}
