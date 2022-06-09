ee09c3524822e69e7f5f3a26c983298be3e78f50
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 09:27:19 +0300

    _element_link: Icon support

---
+++ element/link/link.view.css.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+
+	$mol_style_define( $hyoo_sketch_element_link, {
+
+		Element: {
+			alignItems: 'center',
+		},
+
+	} )
+
+}
--- element/link/link.view.ts
+++ element/link/link.view.ts
@@ -2,6 +2,13 @@ namespace $.$$ {
 
 	export class $hyoo_sketch_element_link extends $.$hyoo_sketch_element_link {
 
+		link_sub() {
+			return [
+				... this.Icon().icon_name() ? [ this.Icon().Icon() ] : [],
+				this.link_title(),
+			]
+		}
+
 		link_hint(next?: string) {
 			return String( this.state().sub('link_hint').value(next) ?? super.link_hint() )
 		}
