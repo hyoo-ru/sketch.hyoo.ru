53c152140666a6d5538276aa06f8a509237b2fa6
Pavel Zubkov <zubkov_p@mail.ru>
2022-06-09 18:29:27 +0300

    _element_textarea: Added

---
+++ element/textarea/textarea.view.ts
@@ -0,0 +1,26 @@
+namespace $.$$ {
+
+	export class $hyoo_sketch_element_textarea extends $.$hyoo_sketch_element_textarea {
+
+		text(next?: string) {
+			return this.state().sub('textarea_text').text(next)
+		}
+
+		hint(next?: string) {
+			return String( this.state().sub('textarea_hint').value(next) ?? '' )
+		}
+
+		duplicate(elem?: $hyoo_sketch_element) {
+			const element = elem ?? super.duplicate()
+			const obj = new $hyoo_sketch_element_textarea
+			obj.element = $mol_const(element)
+
+			obj.text(this.text())
+			obj.hint(this.hint())
+
+			return element
+		}
+
+	}
+
+}
