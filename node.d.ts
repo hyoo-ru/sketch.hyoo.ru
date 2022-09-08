7cc4e6c3b5b05bfbf208f5969741900cece470d5
PavelZubkov <PavelZubkov@users.noreply.github.com>
2022-09-08 07:35:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -4550,7 +4550,7 @@ declare namespace $ {
             src: string;
             srcdoc: any;
             allow: string;
-            allowFullscreen: boolean;
+            allowfullscreen: boolean;
         };
         accelerometer(): boolean;
         autoplay(): boolean;
--- web.d.ts
+++ web.d.ts
@@ -4467,7 +4467,7 @@ declare namespace $ {
             src: string;
             srcdoc: any;
             allow: string;
-            allowFullscreen: boolean;
+            allowfullscreen: boolean;
         };
         accelerometer(): boolean;
         autoplay(): boolean;
