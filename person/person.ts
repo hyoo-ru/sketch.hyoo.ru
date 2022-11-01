85187e7b0dcfc87664a8089af1e14dc74555aacf
jin <jin@hyoo.ru>
2022-11-01 21:27:41 +0800

    new changed moment api

--- person/person.ts
+++ person/person.ts
@@ -50,7 +50,7 @@ namespace $ {
 		
 		@ $mol_mem
 		online_time() {
-			const stamp = this.land().clock_data.last_stamp()
+			const stamp = this.land().last_stamp()
 			return stamp ? new $mol_time_moment( stamp ) : null
 		}
 		
