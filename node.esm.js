4787bffca556708d6a18c1628dba9ed6f37d00e9
PavelZubkov <PavelZubkov@users.noreply.github.com>
2022-11-24 07:18:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.esm.js
+++ node.esm.js
@@ -18369,7 +18369,8 @@ var $;
             if (key === 'id')
                 continue;
             if (typeof props[key] === 'string') {
-                ;
+                if (key in node)
+                    node[key] = props[key];
                 node.setAttribute(key, props[key]);
             }
             else if (props[key] &&
