9ff58d02b6d55cca8178822084ff61e520fd83c0
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-03-01 07:18:02 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.d.ts
+++ node.d.ts
@@ -1558,9 +1558,9 @@ declare namespace $ {
         static href_normal(): string;
         static dict(next?: {
             [key: string]: string | null;
-        }): {
+        }): Readonly<{
             [key: string]: string;
-        };
+        }>;
         static value(key: string, next?: string | null): string | null;
         static link(next: any): string;
         static make_link(next: {
@@ -2578,6 +2578,7 @@ declare namespace $ {
         length_max(): number;
         type(val?: any): string;
         event_change(event?: any): any;
+        submit_with_ctrl(): boolean;
         submit(event?: any): any;
         Submit(): $$.$mol_hotkey;
     }
@@ -2609,20 +2610,22 @@ declare namespace $ {
     class $mol_number extends $mol_view {
         precision_view(): number;
         precision_change(): number;
-        value(val?: any): number;
+        value_min(): number;
+        value_max(): number;
+        value(next?: any): number;
+        enabled(): boolean;
         sub(): readonly any[];
         precision(): number;
         type(): string;
         value_string(val?: any): string;
         hint(): string;
-        enabled(): boolean;
         string_enabled(): boolean;
         String(): $$.$mol_string;
-        event_dec(val?: any): any;
+        event_dec(next?: any): any;
         dec_enabled(): boolean;
         dec_icon(): $mol_icon_minus;
         Dec(): $mol_button_minor;
-        event_inc(val?: any): any;
+        event_inc(next?: any): any;
         inc_enabled(): boolean;
         inc_icon(): $mol_icon_plus;
         Inc(): $mol_button_minor;
@@ -2634,9 +2637,12 @@ declare namespace $ {
 
 declare namespace $.$$ {
     class $mol_number extends $.$mol_number {
+        value_limited(next?: any): number;
         event_dec(next?: Event): void;
         event_inc(next?: Event): void;
         value_string(next?: string): string;
+        dec_enabled(): boolean;
+        inc_enabled(): boolean;
     }
 }
 
@@ -2814,6 +2820,9 @@ declare namespace $ {
     class $mol_fetch_response extends $mol_object2 {
         readonly native: Response;
         constructor(native: Response);
+        status(): "success" | "unknown" | "inform" | "redirect" | "wrong" | "failed";
+        code(): number;
+        message(): string;
         headers(): Headers;
         mime(): string | null;
         stream(): ReadableStream<Uint8Array> | null;
@@ -2829,6 +2838,7 @@ declare namespace $ {
             destructor: () => void;
         };
         static response(input: RequestInfo, init?: RequestInit): $mol_fetch_response;
+        static success(input: RequestInfo, init?: RequestInit): $mol_fetch_response;
         static stream(input: RequestInfo, init?: RequestInit): ReadableStream<Uint8Array> | null;
         static text(input: RequestInfo, init?: RequestInit): string;
         static json(input: RequestInfo, init?: RequestInit): unknown;
@@ -4307,6 +4317,7 @@ declare namespace $ {
         spellcheck(): boolean;
         length_max(): number;
         selection(val?: any): readonly number[];
+        submit(next?: any): any;
         bring(): void;
         Edit(): $mol_textarea_edit;
         row_numb(id: any): number;
