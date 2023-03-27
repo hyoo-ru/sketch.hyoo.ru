47f74d13a20a2e6dd54346405169964d2e39b045
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-03-27 07:18:04 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -15341,11 +15341,14 @@ var $;
 var $;
 (function ($) {
     class $mol_button_copy extends $mol_button_minor {
+        blobs() {
+            return [
+                this.text_blob(),
+                this.html_blob()
+            ];
+        }
         data() {
-            return {
-                "text/plain": this.text_blob(),
-                "text/html": this.html_blob()
-            };
+            return {};
         }
         sub() {
             return [
@@ -15422,6 +15425,9 @@ var $;
     var $$;
     (function ($$) {
         class $mol_button_copy extends $.$mol_button_copy {
+            data() {
+                return Object.fromEntries(this.blobs().map(blob => [blob.type, blob]));
+            }
             html() {
                 return $mol_html_encode(this.text());
             }
