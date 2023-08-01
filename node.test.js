65409c57441cbd1fc373693de18bebd45915f643
PavelZubkov <PavelZubkov@users.noreply.github.com>
2023-08-01 07:16:43 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.test.js
+++ node.test.js
@@ -20330,7 +20330,8 @@ var $;
             let res = {};
             for (const field in sub) {
                 try {
-                    res[field] = sub[field](val[field]);
+                    res[field] =
+                        sub[field](val[field]);
                 }
                 catch (error) {
                     if (error instanceof Promise)
